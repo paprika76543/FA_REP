3c7c5f174239f246575daefda82bdacdfbe01945
paprika76543 <153736861+paprika76543@users.noreply.github.com>
2023-12-29 15:07:04 +0300

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -20,7 +20,17 @@ def test_predict2_negative():
     assert response.status_code == 200
     assert json_data['label'] == 'NEGATIVE'
 
-def test_predict_negative():
+def test_predict_correct_answer():
     response = client.post("/predict/",
                            json={"text": "I hate machine learning!"})
     assert response.status_code == 200
+
+def test_predict_not_empty_answer():
+    response = client.post("/predict/",
+                           json={"text": "I hate machine learning!"})
+    json_data = response.json()
+    assert response.status_code == 200
+    assert (len(json_data)) >= 1
+    
+
+
